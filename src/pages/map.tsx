c12e99eb50686aa7b5493c76e0f8da5f8cedb0e7
joshnice <joshliamnice@gmail.com>
2023-07-13 08:58:32 +0100

    Append commit

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,11 +1,13 @@
+import { useRef } from "react";
 import { InputLocationComponent } from "../components/input";
-import { MapComponent } from "../components/map";
+import { MapComponent, MapRef } from "../components/map";
 
 export const MapPage = () => {
+    const mapRef = useRef<MapRef>();
     return (
         <div className="flex w-full h-full">
             <div className="w-4/5 h-full">
-                <MapComponent />
+                <MapComponent ref={mapRef} />
             </div>
             <div className="flex flex-grow flex-col p-4 gap-4">
                 <div className="flex flex-col gap-2 w-full">
@@ -15,6 +17,9 @@ export const MapPage = () => {
                 <div className="flex flex-col gap-2 w-full">
                     Final Destination
                     <InputLocationComponent />
+                </div>
+                <div>
+                    <button onClick={() => mapRef.current?.startRoute()}>Start</button>
                 </div>
             </div>
         </div>
